7f8da1ab9420f1ce2e8844436447d164c830b120
Hannah Olukoye <hannah.olukoye@macro-eyes.com>
2019-10-25 10:19:07 +0300

    Feat: Using roboelectric for testing on Android.

--- app/src/test/java/com/example/android_testing_project/MainActivityTest.java
+++ app/src/test/java/com/example/android_testing_project/MainActivityTest.java
@@ -1,5 +1,8 @@
 package com.example.android_testing_project;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.view.inputmethod.EditorInfo;
 
@@ -8,9 +11,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadow.api.Shadow;
+import org.robolectric.shadows.ShadowActivity;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest=Config.NONE, sdk = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivityTest {
@@ -37,5 +45,33 @@ public class MainActivityTest {
         assertEquals(givenString, actualString);
 
     }
+    @Test
+    public  void spinnerUpdatesBackgroundColor() throws Exception {
+        //Arrange
+        int index = 2;
+        int givenColor = Color.GREEN;
 
+        //Act
+        activity.colorSpinner.setSelection(index);
+
+        //Assert
+        int actualColor = ((ColorDrawable)activity.linearLayout.getBackground()).getColor();
+        assertEquals(givenColor, actualColor);
+    }
+
+    @Test
+    public  void buttonLaunchesOtherActivity() throws Exception {
+        //Arrange
+        Class clazz = OtherActivity.class;
+        Intent expectedIntent = new Intent(activity, clazz);
+
+        //Act
+        activity.launchActivityButton.callOnClick();
+
+        //Assert
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+
+        assertTrue(expectedIntent.filterEquals(actualIntent));
+    }
 }
